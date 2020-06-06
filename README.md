187bb7a91d3b61c6407b967d6273275a72ad642f
Etam <etammao@gmail.com>
2020-06-06 13:09:37 -0400

    Weather Dashboard

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Weather-Dashboard
+# 06 Server-Side APIs: Weather Dashboard
+
+
+Used the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+
+## User Story
+
+```
+AS A traveler
+You can see the weather outlook for multiple cities
+
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+WHEN I open the weather dashboard
+THEN I am presented with the last searched city forecast
+```
+
--- javascript.js
+++ javascript.js
@@ -1,73 +1,3 @@
-
-
-// // async
-//  function seachCity(){
-//     event.preventDefault;
-//     console.log('searchBtn works');
-//     var cityName= document.querySelector('#searchInput').value;
-//     console.log(cityName);
-//     // document.querySelector('#cityNameResult').innerHTML= cityName;
-    
-//     //try  try{
-//     // const AmazonData = await fetchAmazonData( consoleName );
-//     // const GoogleData = await fetchGoogleData( consoleName );
-//     // //Display product info
-//     // displayProduct( AmazonData, GoogleData );
-//     // } catch( e ){
-//     //     console.log( `Fetching failed`, e )
-//     // }
-    
-// }
-// var api='https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5534726c08a3a7f6f9639cde7b366630';
-
-// async function fetchWeatherData(api){
-//     console.log('fetch the weather');
-//     var result= await fetch(api).then(result=>result.json());
-
-//     result.forEach(function(cityInfor){
-//         document.querySelector('#todayWeather').innerHTML=' <ul id="todayWeather" class="list-group">
-//         <h1 id="cityNameResult" class="list-group-item"></h1>
-//         <li class="list-group-item">conditionsTORONTO</li>
-//         <li class="list-group-item">temperature</li>
-//         <li class="list-group-item">humidity</li>
-//         <li class="list-group-item">wind speed</li>
-//         <li class="list-group-item">UV index</li>
-//       </ul>'
-    
-        
-//     });
-
-// }
-
-
-
-// function displayWeather
-
-
-
-
-
-
-
-
-// async function fetchWeather(cityName){
-
-//     var api='https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5534726c08a3a7f6f9639cde7b366630';
-  
-
-// const weatherResult = await fetch(queryURL).then((result)=>result.json());
-// console.log(`weatherresult`,weatherResult);
-// // if(weatherResult){
-// //     displayWeather(weatherResult);
-// // }
-// };
-
-// function displayWeather(currentData){
-//     console.log(currentData);
-    
-
-// }
-
 var time = new Date();
 document.getElementById("timeshown").innerHTML = time;
 
@@ -76,6 +6,14 @@ const temperatureEl=document.querySelector('#temperature');
 const humidityEl=document.querySelector('#humidity');
 const windSpeedEl=document.querySelector('#windSpeed');
 const UVEl=document.querySelector('#UV');
+const forecastDateEl=document.querySelector('#forecastDate');
+const forecastTempEl=document.querySelector('#forecastTemp');
+const forecastHumEl=document.querySelector('#forecastHum');
+const historyEl=document.querySelector('#history');
+let searchHistory = localStorage.searchHistory ? JSON.parse(localStorage.searchHistory) : [];
+
+
+
 
 function getCityName(){
     event.preventDefault;
@@ -93,6 +31,9 @@ async function fetchWeather(cityName){
     let apiOneCall= `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherResult.coord.lat}&lon=${weatherResult.coord.lon}&exclude=hourly,daily&appid=5534726c08a3a7f6f9639cde7b366630`
     const oneCallResult= await fetch(apiOneCall).then((result)=>result.json());
 
+    // let apiForecast=`https://samples.openweathermap.org/data/2.5/forecast/daily?id=${weatherResult.id}&appid=5534726c08a3a7f6f9639cde7b366630`;
+    // const apiForecastResult= await fetch(apiForecast).then((result)=>result.json());
+
     if(weatherResult){
         displayWeather(weatherResult,oneCallResult);
     }else{
@@ -107,5 +48,33 @@ function displayWeather(currentData, oneCallData){
     windSpeedEl.textContent=`wind speed: ${currentData.wind.speed}`;
     humidityEl.textContent=`Humidity: ${currentData.main.humidity}`;
     UVEl.textContent=`UV index: ${oneCallData.current.uvi}`;
+    // let Date= moment().add(1,'days').format('M/D/YYYY');
+    // forecastDateEl.textContent=`${Date}`;
+    // forecastTempEl.textContent=`Temperature:${currentData.main.temp}`;
+    // forecastHumEl.textContent=`Humidity:${currentData.main.humidity}`;
 
+    searchHistory.push(`${currentData.name}`);
+    localStorage.searchHistory = JSON.stringify(searchHistory);
+    historyFun();
+    // forecastData.list.forEach(displayForecast);
+
+}
+
+// function displayForecast(item){
+// let Date= moment().add(1,'days').format('M/D/YYYY');
+// document.querySelector('#forecast').innerHTML+=
+
+// }
+
+function clearHistory(){
+    localStorage.clear();
+    historyEl.innerHTML='';
+}
+
+
+function historyFun(){
+    historyEl.innerHTML='';
+    searchHistory.forEach(function(item){
+        historyEl.innerHTML += `<li class="list-group-item" onclick="fetchWeather( '${item}' )">${item}</li>`
+    })
 }
