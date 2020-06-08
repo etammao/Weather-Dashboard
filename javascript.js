44ffae66778cf25ab2faddcc6d30efa644583af0
Etam <etammao@gmail.com>
2020-06-08 19:13:45 -0400

    Weather-Dashboard completed

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ function getCityName(){
     console.log('works')
     let cityName=document.querySelector('#searchInput').value;
     console.log(cityName);
-    // document.querySelector('#location').innerHTML= cityName;
+  
     return cityName;
 }
 
@@ -31,42 +31,92 @@ async function fetchWeather(cityName){
     let apiOneCall= `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherResult.coord.lat}&lon=${weatherResult.coord.lon}&exclude=hourly,daily&appid=5534726c08a3a7f6f9639cde7b366630`
     const oneCallResult= await fetch(apiOneCall).then((result)=>result.json());
 
-    // let apiForecast=`https://samples.openweathermap.org/data/2.5/forecast/daily?id=${weatherResult.id}&appid=5534726c08a3a7f6f9639cde7b366630`;
-    // const apiForecastResult= await fetch(apiForecast).then((result)=>result.json());
-
+    let apiForecast=`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=5534726c08a3a7f6f9639cde7b366630`;
+    const apiForecastResult= await fetch(apiForecast).then((result)=>result.json());
+debugger
+    
     if(weatherResult){
-        displayWeather(weatherResult,oneCallResult);
+        displayWeather(weatherResult,oneCallResult,apiForecastResult);
     }else{
         document.querySelector('#todayWeather').innerHTML=`<h1>Please try again</h1>`;
 
     }
 }
 
-function displayWeather(currentData, oneCallData){
-    var tempNum= currentData.main.temp - 273.15;
-    var tempNumber=Number(tempNum).toFixed(1);
+const forecastDate1El= document.querySelector('#forecastDate1');
+const forecastDate2El= document.querySelector('#forecastDate2');
+const forecastDate3El= document.querySelector('#forecastDate3');
+const forecastDate4El= document.querySelector('#forecastDate4');
+const forecastDate5El= document.querySelector('#forecastDate5');
+
+const forecastTemp1El= document.querySelector('#forecastTemp1');
+const forecastTemp2El= document.querySelector('#forecastTemp2');
+const forecastTemp3El= document.querySelector('#forecastTemp3');
+const forecastTemp4El= document.querySelector('#forecastTemp4');
+const forecastTemp5El= document.querySelector('#forecastTemp5');
+
+const forecastHum1El= document.querySelector('#forecastHum1');
+const forecastHum2El= document.querySelector('#forecastHum2');
+const forecastHum3El= document.querySelector('#forecastHum3');
+const forecastHum4El= document.querySelector('#forecastHum4');
+const forecastHum5El= document.querySelector('#forecastHum5');
+
+
+
+function displayWeather(currentData, oneCallData,ForecastData){
+    var tempNumCurrent= currentData.main.temp - 273.15;
+    var tempNumberCurrent=Number(tempNumCurrent).toFixed(1);
+
+    var tempNumforecast1= ForecastData.list[6].main.temp - 273.15;
+    var tempNumberforecast1=Number(tempNumforecast1).toFixed(1);
+
+    var tempNumforecast2= ForecastData.list[14].main.temp - 273.15;
+    var tempNumberforecast2=Number(tempNumforecast2).toFixed(1);
+
+    var tempNumforecast3= ForecastData.list[22].main.temp - 273.15;
+    var tempNumberforecast3=Number(tempNumforecast3).toFixed(1);
+
+    var tempNumforecast4= ForecastData.list[30].main.temp - 273.15;
+    var tempNumberforecast4=Number(tempNumforecast4).toFixed(1);
+
+    var tempNumforecast5= ForecastData.list[38].main.temp - 273.15;
+    var tempNumberforecast5=Number(tempNumforecast5).toFixed(1);
+
+
+    
     locationEl.textContent=`City: ${currentData.name}`;
-    temperatureEl.textContent=`Temperature: ${tempNumber}`;
+    temperatureEl.textContent=`Temperature: ${tempNumberCurrent}`;
     windSpeedEl.textContent=`wind speed: ${currentData.wind.speed}`;
     humidityEl.textContent=`Humidity: ${currentData.main.humidity}`;
     UVEl.textContent=`UV index: ${oneCallData.current.uvi}`;
-    // let Date= moment().add(1,'days').format('M/D/YYYY');
-    // forecastDateEl.textContent=`${Date}`;
-    // forecastTempEl.textContent=`Temperature:${currentData.main.temp}`;
-    // forecastHumEl.textContent=`Humidity:${currentData.main.humidity}`;
 
+    forecastDate1El.textContent=`Date: ${ForecastData.list[6].dt_txt}`;
+    forecastDate2El.textContent=`Date: ${ForecastData.list[14].dt_txt}`;
+    forecastDate3El.textContent=`Date: ${ForecastData.list[22].dt_txt}`;
+    forecastDate4El.textContent=`Date: ${ForecastData.list[30].dt_txt}`;
+    forecastDate5El.textContent=`Date: ${ForecastData.list[38].dt_txt}`;
+
+    forecastTemp1El.textContent=`Temperature: ${tempNumberforecast1}`;
+    forecastTemp2El.textContent=`Temperature: ${tempNumberforecast2}`;
+    forecastTemp3El.textContent=`Temperature: ${tempNumberforecast3}`;
+    forecastTemp4El.textContent=`Temperature: ${tempNumberforecast4}`;
+    forecastTemp5El.textContent=`Temperature: ${tempNumberforecast5}`;
+
+    forecastHum1El.textContent=`humidity: ${ForecastData.list[6].main.humidity}`;
+    forecastHum2El.textContent=`humidity: ${ForecastData.list[14].main.humidity}`;
+    forecastHum3El.textContent=`humidity: ${ForecastData.list[22].main.humidity}`;
+    forecastHum4El.textContent=`humidity: ${ForecastData.list[30].main.humidity}`;
+    forecastHum5El.textContent=`humidity: ${ForecastData.list[38].main.humidity}`;
+
+   
     searchHistory.push(`${currentData.name}`);
     localStorage.searchHistory = JSON.stringify(searchHistory);
     historyFun();
-    // forecastData.list.forEach(displayForecast);
+  
 
 }
 
-// function displayForecast(item){
-// let Date= moment().add(1,'days').format('M/D/YYYY');
-// document.querySelector('#forecast').innerHTML+=
 
-// }
 
 function clearHistory(){
     localStorage.clear();
